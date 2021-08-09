b8ca236f337ce515d89edf036d90762d7b6d919c
ADSirakova93 <adsirakova.93@gmail.com>
2021-08-09 06:13:16 +0300

    Update

--- 03. Characters in Range/Program.cs
+++ 03. Characters in Range/Program.cs
@@ -6,7 +6,25 @@ namespace _03._Characters_in_Range
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char start = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+
+            PrintCharacterinRange(start, end);
+        }
+
+        private static void PrintCharacterinRange(char start, char end)
+        {
+            //ako kraq e po-golqm ot nachaloto
+            if (end < start)
+            {
+                char temp = start;
+                start = end;
+                end = temp;
+            }
+            for (char i = (char)(start + 1); i < end; i++)
+            {
+                Console.Write(i + " ");
+            }
         }
     }
 }
--- VowelsCount/Program.cs
+++ VowelsCount/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace VowelsCount
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            Console.WriteLine(VowelsCount(input));
+        }
+        private static int VowelsCount(string input)
+        {
+            int vowelsCCount = 0;
+            //a o e u i y
+            string inputToLower = input.ToLower();
+            for (int i = 0; i < inputToLower.Length; i++)
+            {
+                if (inputToLower[i] == 'a' ||
+                    inputToLower[i] == 'e' ||
+                    inputToLower[i] == 'u' ||
+                    inputToLower[i] == 'o' ||
+                    inputToLower[i] == 'i' ||
+                    inputToLower[i] == 'y')
+                {
+                    vowelsCCount++;
+                }
+
+            }
+            return vowelsCCount;
+
         }
     }
 }
